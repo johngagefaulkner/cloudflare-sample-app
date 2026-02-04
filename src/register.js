b24f9a3a08bca7c1a3cc746b66cfb6da57127ace
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2026-02-04 10:49:17 -0500

    Add generate-image modal command

--- src/register.js
+++ src/register.js
@@ -1,4 +1,8 @@
-import { AWW_COMMAND, INVITE_COMMAND } from './commands.js';
+import {
+  AWW_COMMAND,
+  GENERATE_IMAGE_COMMAND,
+  INVITE_COMMAND,
+} from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
@@ -34,7 +38,7 @@ const response = await fetch(url, {
     Authorization: `Bot ${token}`,
   },
   method: 'PUT',
-  body: JSON.stringify([AWW_COMMAND, INVITE_COMMAND]),
+  body: JSON.stringify([AWW_COMMAND, INVITE_COMMAND, GENERATE_IMAGE_COMMAND]),
 });
 
 if (response.ok) {
